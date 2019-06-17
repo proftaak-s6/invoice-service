38213822f2074cb783da1836ac3505411fbf4e06
reinoudvanzoelen <reinoud95@hotmail.com>
2019-06-17 08:58:09 +0200

    trackservice

--- src/main/java/services/TrackServiceImpl.java
+++ src/main/java/services/TrackServiceImpl.java
@@ -3,14 +3,22 @@ package services;
 import java.time.Month;
 import java.util.List;
 
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+
 import models.trackservice.Step;
 import services.interfaces.TrackService;
 
-public class TrackServiceImpl implements TrackService {
+public class TrackServiceImpl extends BaseClientService implements TrackService {
+
+    private static final String BASE_URI = "http://track.fontys-project.nl/";
 
     @Override
     public List<Step> getDriven(long trackerId, int year, Month month) {
-        return null;
+        List<Step> steps = getClient().target(BASE_URI + "/" + trackerId + "/" + year + "/" + month)
+                .request(MediaType.APPLICATION_JSON).get(new GenericType<List<Step>>() {
+                });
+        return steps;
     }
 
 }
