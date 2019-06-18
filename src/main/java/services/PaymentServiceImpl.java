812364f73b6f680390da55cb4e87d2c71f3b7839
reinoudvanzoelen <reinoud95@hotmail.com>
2019-06-18 13:29:27 +0200

    payment service implementation

--- src/main/java/services/PaymentServiceImpl.java
+++ src/main/java/services/PaymentServiceImpl.java
@@ -12,76 +12,45 @@ import java.util.List;
 
 public class PaymentServiceImpl implements PaymentService {
 
+    @PersistenceContext(unitName = "PU")
+    private EntityManager entityManager;
+
     @Override
     public Payment getById(long id) {
-        return null;
+        return entityManager.find(Payment.class, id);
     }
 
     @Override
     public List<Payment> getAll() {
-        return null;
+        return entityManager.createQuery("SELECT p FROM Payment p", Payment.class).getResultList();
     }
 
     @Override
+    @Transactional
     public Payment create(Payment model) {
-        return null;
+        entityManager.persist(model);
+        return model;
     }
 
     @Override
+    @Transactional
     public Payment update(Payment model) {
-        return null;
+        entityManager.merge(model);
+        return model;
     }
 
     @Override
+    @Transactional
     public boolean delete(long id) {
-        return false;
+        Payment paymentToDelete = entityManager.find(Payment.class, id);
+        entityManager.remove(paymentToDelete);
+        return true;
     }
 
     @Override
     public Payment createIfNew(String bsn, int year, Month month) {
+        // TODO!
         return null;
     }
-    
-    // @PersistenceContext(unitName = "PU")
-    // private EntityManager entityManager;
-
-    // @Override
-    // public Payment getById(long id) {
-    // return entityManager.find(Payment.class, id);
-    // }
-
-    // @Override
-    // public List<Payment> getAll() {
-    // return entityManager.createQuery("SELECT p FROM Payment p",
-    // Payment.class).getResultList();
-    // }
-
-    // @Override
-    // @Transactional
-    // public Payment create(Payment model) {
-    // entityManager.persist(model);
-    // return model;
-    // }
-
-    // @Override
-    // @Transactional
-    // public Payment update(Payment model) {
-    // entityManager.merge(model);
-    // return model;
-    // }
-
-    // @Override
-    // @Transactional
-    // public boolean delete(long id) {
-    // Payment paymentToDelete = entityManager.find(Payment.class, id);
-    // entityManager.remove(paymentToDelete);
-    // return true;
-    // }
-
-    // @Override
-    // public Payment createIfNew(String bsn, int year, Month month) {
-    // // TODO!
-    // return null;
-    // }
 
 }
